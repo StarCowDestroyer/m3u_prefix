37b326f39dd607efb7c43230ebbf2b0721905dd0
Giacomo <jackmeng2.7@gmail.com>
2025-10-25 00:14:18 +0200

    replaceprefix.c replace in single m3u file and replace in all m3u files

---
+++ src/replaceprefix.c
@@ -0,0 +1,5 @@
+#include "../include/replaceprefix.h"
+
+// replaces prefixes in only one m3u file
+int replace_prefix(char *m3u_file, char *replace_str);
+int replace_prefixes_all(char **m3u_files, char *replace_str);
