454e74f280b9fae6b9155e7a08cf6d5e0b400f3b
Giacomo <jackmeng2.7@gmail.com>
2025-10-29 22:17:35 +0100

    Riscritto grazie a llm

--- src/replaceprefix.c
+++ src/replaceprefix.c
@@ -34,53 +34,110 @@ char *build_file_path(char *dir_path, char *file_name) {
 
 char *copym3ufile(char *m3u_dir, char *m3u_file) {
     char *path_to_file = build_file_path(m3u_dir, m3u_file);
-    FILE *m3u_f = fopen(path_to_file, "r");
-    if (m3u_f == NULL) {
-        printf("ERROR\nIn replace_prefix.\nFailed to open %s\n.", m3u_file);
+    if (!path_to_file) {
+        return NULL;
     }
 
-    char *file_str = (char *)malloc(BUFFER_REALLOC_SIZE);
-    file_str[0] = '\0';
-    size_t file_str_size = 0;
-    char buffer[BUFFER_REALLOC_SIZE];
-    while (fgets(buffer, BUFFER_REALLOC_SIZE - 1, m3u_f)) {
-        size_t buf_str_size = strlen(buffer);
-        char *p = (char *)realloc(file_str, buf_str_size + file_str_size + 1);
-        if (!p) {
-            printf("ERROR\ncouldnt realloc memory for file_str\n");
-            free(p);
-            fclose(m3u_f);
-            free(path_to_file);
-        }
-        file_str = p;
-        strcat(file_str, buffer);
-        file_str_size += buf_str_size;
-        file_str[file_str_size] = '\0';
+    FILE *m3u_f = fopen(path_to_file, "r");
+    if (m3u_f == NULL) {
+        printf("ERROR\nIn replace_prefix.\nFailed to open %s\n.", path_to_file);
+        free(path_to_file);
+        return NULL;
     }
+
+    // char *file_str = (char *)malloc(BUFFER_REALLOC_SIZE);
+    // file_str[0] = '\0';
+    // size_t file_str_size = 0;
+    // char buffer[BUFFER_REALLOC_SIZE];
+    // while (fgets(buffer, BUFFER_REALLOC_SIZE - 1, m3u_f)) {
+    //     size_t buf_str_size = strlen(buffer);
+    //     char *p = (char *)realloc(file_str, buf_str_size + file_str_size +
+    //     1); if (!p) {
+    //         printf("ERROR\ncouldnt realloc memory for file_str\n");
+    //         free(p);
+    //         fclose(m3u_f);
+    //         free(path_to_file);
+    //     }
+    //     file_str = p;
+    //     strcat(file_str, buffer);
+    //     file_str_size += buf_str_size;
+    //     file_str[file_str_size] = '\0';
+    // }
+    // fclose(m3u_f);
+    //
+
+    // get file size
+    fseek(m3u_f, 0, SEEK_END);
+    size_t file_size = ftell(m3u_f);
+    fseek(m3u_f, 0, SEEK_SET);
+
+    // Allocate buffer for entire file
+    char *file_str = (char *)malloc(file_size + 1);
+    if (!file_str) {
+        printf("ERROR\nFailed to allocate memory for file content\n");
+        fclose(m3u_f);
+        free(path_to_file);
+        return NULL;
+    }
+
+    // read entire file at once
+    size_t bytes_read = fread(file_str, 1, file_size, m3u_f);
+    file_str[bytes_read] = '\0';
+
     fclose(m3u_f);
+    free(path_to_file);
 
     return file_str;
 }
 
 // replaces prefixes in only one m3u file
 int replace_prefix(char *m3u_dir, char *m3u_file, char *prefix, char *replace) {
-    // apri file in lettura, copia tutto in una stringa, usa stringutils per
-    // sostituire apri lo stesso file in scrittura e copia la stringa modificata
+    // // apri file in lettura, copia tutto in una stringa, usa stringutils per
+    // // sostituire apri lo stesso file in scrittura e copia la stringa
+    // modificata char *file_str = copym3ufile(m3u_dir, m3u_file);
+    // printf("COPIATO\n");
+    // printf("FILE:\n%s\n", file_str);
+    //
+    // strreplace(&file_str, prefix, replace);
+    // printf("RIMPIAZZATO\n");
+    //
+    // // char *file_path = build_file_path(m3u_dir, m3u_file);
+    // // FILE *m3u_f = fopen(file_path, "w");
+    // // fprintf(m3u_f, file_str);
+    //
+    // printf("FILE:\n%s", file_str);
+    // free(file_str);
     char *file_str = copym3ufile(m3u_dir, m3u_file);
-    printf("COPIATO\n");
-    printf("FILE:\n%s\n", file_str);
+    if (!file_str) {
+        return 0;
+    }
 
-    strreplace(&file_str, prefix, replace);
-    printf("RIMPIAZZATO\n");
+    if (!strreplace(&file_str, prefix, replace)) {
+        printf("ERROR\nFailed to replace string in %s\n", m3u_file);
+        free(file_str);
+        return 0;
+    }
 
-    // char *file_path = build_file_path(m3u_dir, m3u_file);
-    // FILE *m3u_f = fopen(file_path, "w");
-    // fprintf(m3u_f, file_str);
+    char *file_path = build_file_path(m3u_dir, m3u_file);
+    if (!file_path) {
+        free(file_str);
+        return 0;
+    }
 
-    printf("FILE:\n%s", file_str);
+    FILE *m3u_f = fopen(file_path, "w");
+    if (!m3u_f) {
+        printf("ERROR\nFailed to open %s for writing\n", file_path);
+        free(file_str);
+        free(file_path);
+        return 0;
+    }
+
+    fprintf(m3u_f, "%s", file_str);
+    fclose(m3u_f);
+    free(file_path);
     free(file_str);
 
-    return 0;
+    return 1;
 }
 
 int replace_prefixes_all(char **m3u_files, int size, char *m3u_dir,
--- src/stringutils.c
+++ src/stringutils.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,49 +24,106 @@ int strfind(char *s1, const char *s2, int starting_point) {
 /*
  * replaces all occurrences of torepl with repl
  */
+// int strreplace(char **file_ptr, const char *torepl, const char *repl) {
+//     int file_length = strlen(*file_ptr);
+//     int torepl_length = strlen(torepl);
+//     int repl_length = strlen(repl);
+//
+//     int indexes[file_length / torepl_length];
+//     int i = 0;
+//
+//     indexes[i] = strfind(*file_ptr, torepl, 0);
+//     while (indexes[i] != -1) {
+//         i++;
+//         indexes[i] = strfind(*file_ptr, torepl, indexes[i - 1] +
+//         torepl_length); printf("indexes[%d] %d\n", i, indexes[i]);
+//     }
+//     // now we have to replace #i words
+//     int new_file_length = file_length - (i * (torepl_length - repl_length));
+//     char *new_file = (char *)malloc(new_file_length);
+//     assert(new_file);
+//
+//     int count = 0;
+//     int i2 = 0;
+//     i = 0;
+//     while (i < new_file_length) {
+//         if (indexes[count] == i) {
+//             // inserisci repl
+//             for (int j = 0; j < repl_length; j++) {
+//                 new_file[j + i] = repl[j];
+//             }
+//             indexes[count + 1] += -(count + 1) * (torepl_length -
+//             repl_length); count++; i += repl_length; i2 += torepl_length;
+//             new_file[i] = '\0';
+//         }
+//         new_file[i] = (*file_ptr)[i2];
+//         new_file[i + 1] = '\0';
+//         i++;
+//         i2++;
+//     }
+//
+//     free(*file_ptr);
+//     *file_ptr = new_file;
+//     (*file_ptr)[new_file_length] = '\0';
+//
+//     return 1;
+// }
 int strreplace(char **file_ptr, const char *torepl, const char *repl) {
-    int file_length = strlen(*file_ptr);
-    int torepl_length = strlen(torepl);
-    int repl_length = strlen(repl);
-
-    int indexes[file_length / torepl_length];
-    int i = 0;
-
-    indexes[i] = strfind(*file_ptr, torepl, 0);
-    while (indexes[i] != -1) {
-        i++;
-        indexes[i] = strfind(*file_ptr, torepl, indexes[i - 1] + torepl_length);
-        printf("indexes[%d] %d\n", i, indexes[i]);
-    }
-    // now we have to replace #i words
-    int new_file_length = file_length - (i * (torepl_length - repl_length));
-    char *new_file = (char *)malloc(new_file_length);
-    assert(new_file);
-
-    int count = 0;
-    int i2 = 0;
-    i = 0;
-    while (i < new_file_length) {
-        if (indexes[count] == i) {
-            // inserisci repl
-            for (int j = 0; j < repl_length; j++) {
-                new_file[j + i] = repl[j];
-            }
-            indexes[count + 1] += -(count + 1) * (torepl_length - repl_length);
-            count++;
-            i += repl_length;
-            i2 += torepl_length;
-            new_file[i] = '\0';
-        }
-        new_file[i] = (*file_ptr)[i2];
-        new_file[i + 1] = '\0';
-        i++;
-        i2++;
+    if (!file_ptr || !*file_ptr || !torepl || !repl) {
+        return 0;
     }
 
+    // size_t e' un unsigned int creato apposta per mantenere tutte le posizioni
+    // indirizzabili di un pc a 64 (o 32) bit e quindi si usa per le size di
+    // array
+    size_t file_length = strlen(*file_ptr);
+    size_t torepl_length = strlen(torepl);
+    size_t repl_length = strlen(repl);
+
+    // count occurrences first
+    size_t count = 0;
+    const char *pos = *file_ptr;
+    while ((pos = strstr(pos, torepl)) != NULL) {
+        count++;
+        pos += torepl_length;
+    }
+
+    if (count == 0) {
+        return 1;
+    }
+
+    // Calculate new size and allocate memory
+    size_t new_length = file_length + count * (repl_length - torepl_length) + 1;
+    char *new_file = (char *)malloc(new_length);
+    if (!new_file) {
+        return 0;
+    }
+
+    // Perform replacement
+    const char *read_pos = *file_ptr;
+    char *write_pos = new_file;
+
+    while ((pos = strstr(read_pos, torepl)) != NULL) {
+        // copy everything up to the match
+        size_t chunk_size = pos - read_pos;
+        memcpy(write_pos, read_pos, chunk_size);
+        write_pos += chunk_size;
+
+        // copy the replacement
+        memcpy(write_pos, repl, repl_length);
+        write_pos += repl_length;
+
+        // move read position past the replaced text
+        read_pos = pos + torepl_length;
+    }
+
+    // copy the remaining part
+    strcpy(write_pos, read_pos);
+
+    // free old buffer and update pointer
     free(*file_ptr);
     *file_ptr = new_file;
-    (*file_ptr)[new_file_length] = '\0';
+    (*file_ptr)[new_length] = '\0';
 
     return 1;
 }
